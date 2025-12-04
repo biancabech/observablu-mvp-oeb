2fdead09272c93ded0ac72d9874f2e034e1e4620
Daniel Stickel <danielstickel21@gmail.com>
2025-12-04 18:52:47 -0300

    configuring tailwind and creating the Home, dashboard, and filter components.

--- front-end/agrupainfo/src/app/app.routes.ts
+++ front-end/agrupainfo/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { Home,  } from './pages/home/home';
+import {  Dashboard} from './pages/dashboard/dashboard';
+import { Filter,  } from './pages/filter/filter';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: 'home', component: Home},
+  { path: 'dashboard', component: Dashboard },
+  { path: 'filter', component: Filter },
+];
